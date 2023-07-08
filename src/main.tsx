e5b829e93d985e971022fb1ea858dd2eb37dec23
Viviana Rodriguez <viviana@Vivianas-MacBook-Air.local>
2023-07-07 20:46:47 -0600

    Add custom font

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "./index.css";
 import { router } from "./router";
 import { UserProvider } from "./contexts/UserContext";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <UserProvider>
-      <div className="flex w-screen h-screen">
+      <div className="flex w-screen h-screen font-face-montserrat">
         <RouterProvider router={router} />
       </div>
     </UserProvider>
